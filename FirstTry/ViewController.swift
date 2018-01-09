1aa305986c1fcd477301c755c26822b1714674e1
Rebecca Fuller <bexter53@hotmail.com>
2018-01-09 14:08:10 +0100

    Initial Commit

---
+++ FirstTry/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirstTry
+//
+//  Created by Rebecca Fuller on 09/01/2018.
+//  Copyright © 2018 Rebecca Fuller. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
