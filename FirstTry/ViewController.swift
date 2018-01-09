fb126b28e293d235a420a87953391e199bfbc559
Rebecca Fuller <bexter53@hotmail.com>
2018-01-09 18:41:38 +0100

    My 2nd commit

--- FirstTry/ViewController.swift
+++ FirstTry/ViewController.swift
@@ -4,15 +4,28 @@
 //
 //  Created by Rebecca Fuller on 09/01/2018.
 //  Copyright © 2018 Rebecca Fuller. All rights reserved.
+// Altlansian are taking over the world!
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var TheLabel: UILabel!
+    var PushCount=0
+    @IBAction func ButtonPushed(_ sender: Any) {
+        PushCount = PushCount + 1
+        print(PushCount)
+        if PushCount > 19{
+            TheLabel.text = "Do we have a problem???"
+        }
+    }
+    
+    @IBAction func NextButtonPushed(_ sender: Any) {
+        TheLabel.text = "OMG! Buttons ROCK!!!!"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loading the view, typically from a nib..
     }
 
     override func didReceiveMemoryWarning() {
